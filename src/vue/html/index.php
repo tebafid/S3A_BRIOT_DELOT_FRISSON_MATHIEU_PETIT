352e0dd1d0624d26ac896c115d53e32b926b0da7
Florian Mathieu <mathieuflo88@gmail.com>
2021-01-20 11:18:40 +0100

    correction titre

--- src/vue/html/index.php
+++ src/vue/html/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html lang="fr">
 <head>
-    <title> MyWishList </title>
+    <title> MyWishlist </title>
     <meta charset='utf-8'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
